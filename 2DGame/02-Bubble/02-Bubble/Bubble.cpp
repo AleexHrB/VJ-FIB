e631072a9240b27ffadf6c9c28a7ef50f1cb1cfc
AleexHrB <alexherrerobravo@gmail.com>
2024-03-11 19:08:45 +0100

    ahora se ha subido

---
+++ 2DGame/02-Bubble/02-Bubble/Bubble.cpp
@@ -0,0 +1,68 @@
+#include <cmath>
+#include <iostream>
+#include <GL/glew.h>
+#include "Bubble.h"
+#include "Game.h"
+
+
+#define JUMP_ANGLE_STEP 4
+#define JUMP_HEIGHT 96
+#define FALL_STEP 4
+
+
+enum PlayerAnims
+{
+	STAND_LEFT, STAND_RIGHT, MOVE_LEFT, MOVE_RIGHT, SHOOT, SIZE
+};
+
+
+void Bubble::init(const glm::ivec2& tileMapPos, ShaderProgram& shaderProgram)
+{
+	
+	spritesheet.loadFromFile("images/Bubble.png", TEXTURE_PIXEL_FORMAT_RGBA);
+	sprite = Sprite::createSprite(glm::ivec2(48, 40), glm::vec2(1, 1/3), &spritesheet, &shaderProgram);
+	tileMapDispl = tileMapPos;
+	speed.x = 4;
+	speed.y = 4;
+	posPlayer.x = 20;
+	posPlayer.y = 20;
+	sprite->setPosition(glm::vec2(float(tileMapDispl.x + posPlayer.x), float(tileMapDispl.y + posPlayer.y)));
+}
+
+void Bubble::update(int deltaTime)
+{
+	sprite->update(deltaTime);
+	posPlayer.x += speed.x;
+	posPlayer.y += speed.y;
+	
+	if (map->collisionMoveDown(posPlayer, glm::ivec2(32, 32), &posPlayer.y)) {
+		posPlayer.y = posPlayer.y - 2*speed.y;
+		speed.y = speed.y * -1;
+	}
+
+	if (map->collisionMoveLeft(posPlayer, glm::ivec2(32, 32)) || map->collisionMoveRight(posPlayer, glm::ivec2(32, 32)))
+	{
+		posPlayer.x = posPlayer.x - 2*speed.x;
+		speed.x = speed.x * -1;
+	}
+
+	
+
+	sprite->setPosition(glm::vec2(float(tileMapDispl.x + posPlayer.x), float(tileMapDispl.y + posPlayer.y)));
+}
+
+void Bubble::render()
+{
+	sprite->render();
+}
+
+void Bubble::setTileMap(TileMap* tileMap)
+{
+	map = tileMap;
+}
+
+void Bubble::setPosition(const glm::vec2& pos)
+{
+	posPlayer = pos;
+	sprite->setPosition(glm::vec2(float(tileMapDispl.x + posPlayer.x), float(tileMapDispl.y + posPlayer.y)));
+}
