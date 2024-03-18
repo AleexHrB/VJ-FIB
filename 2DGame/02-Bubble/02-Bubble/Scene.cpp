9409886006e00a4a8e9e73c42e4d79b465f3c240
lluc.clavera <lluc.clavera@estudiantat.upc.edu>
2024-03-18 16:32:42 +0100

    todo lo que hemos hecho hoy

--- 2DGame/02-Bubble/02-Bubble/Scene.cpp
+++ 2DGame/02-Bubble/02-Bubble/Scene.cpp
@@ -31,16 +31,19 @@ Scene::~Scene()
 
 void Scene::init(unsigned int level)
 {
+	this->menu = level == 0;
 	initShaders();
 	this->level = level;
 	string levelFile = "levels/level" + to_string(level) + ".txt";
 	map = TileMap::createTileMap(levelFile, glm::vec2(SCREEN_X, SCREEN_Y), texProgram);
 	player = new Player();
+	for (Bubble* b : l) delete b;
 	l = list<Bubble*>();
 	l_e = list<Enemy*>();
 	l.push_back(new Bubble());
 	player->init(glm::ivec2(SCREEN_X, SCREEN_Y), texProgram);
-	l.back()->init(texProgram, glm::ivec2(320, 320), glm::ivec2(-4, 0), glm::ivec2(48,40), l.back() -> BLUE);
+	if (menu) l.back()->init(texProgram, glm::ivec2(320, 320), glm::ivec2(-4, 0), glm::ivec2(48,40), l.back() -> BLUE);
+	else l.back()->init(texProgram, glm::ivec2(320, 120), glm::ivec2(-4, 0), glm::ivec2(48, 40), l.back()->BLUE);
 	player->setPosition(glm::vec2(INIT_PLAYER_X_TILES * map->getTileSize(), INIT_PLAYER_Y_TILES * map->getTileSize()));
 	player->setTileMap(map);
 	//bubble->setPosition(glm::vec2(INIT_PLAYER_X_TILES * map->getTileSize(), INIT_PLAYER_Y_TILES * map->getTileSize()));
@@ -53,9 +56,9 @@ void Scene::init(unsigned int level)
 	//if(!text.init("fonts/OpenSans-Bold.ttf"))
 	//if(!text.init("fonts/DroidSerif.ttf"))
 	cout << "Could not load font!!!" << endl;
-	this->menu = level == 0;
+	
 	score = 0;
-	timeLimit = 10;
+	timeLimit = 100;
 }
 
 void Scene::update(int deltaTime)
@@ -78,7 +81,7 @@ void Scene::update(int deltaTime)
 	while (it != l.end()) {
 		Bubble* bub = *it;
 		bub -> update(deltaTime);
-		if (bub->circle_test(player->getPosition())) {
+		if (bub->circle_test(player->getPosition()) && hitted()) {
 			cout << "hit" << endl;
 			bool dead = player->substract_live();
 			if (dead) {
@@ -86,7 +89,7 @@ void Scene::update(int deltaTime)
 				exit(0);
 			}
 		}
-		if (player -> hook_test(bub -> getPosition(), bub -> getSize()) && hitted()) {
+		if (player -> hook_test(bub -> getPosition(), bub -> getSize()) ) {
 			player->setShoot(false);
 			
 			if ((bub->getSize() / 2).x > 6) {
@@ -173,7 +176,7 @@ void Scene::initShaders()
 
 inline bool Scene::hitted()
 {
-	if (currentTime - inmuneTime > 100) {
+	if (currentTime - inmuneTime > 500) {
 		inmuneTime = currentTime;
 		return true;
 	}
