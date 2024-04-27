2545a3b8230a0172801f96daec774ccdcbd1cc66
lluc.clavera <lluc.clavera@estudiantat.upc.edu>
2024-04-27 11:08:47 +0200

    Actualización a dia 25 de abril

--- 3DGame/Assets/Scripts/AnimationControllerScript.cs
+++ 3DGame/Assets/Scripts/AnimationControllerScript.cs
@@ -6,7 +6,8 @@ public class AnimationControllerScript : MonoBehaviour
 {
     // Start is called before the first frame update
     public Animator anim;
-    
+    public AudioClip jump;
+
     void Start()
     {
         anim = GetComponent<Animator>();
@@ -18,6 +19,8 @@ public class AnimationControllerScript : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) ||Input.GetKeyDown(KeyCode.UpArrow) && anim.GetCurrentAnimatorStateInfo(0).IsName("mixamo_correr"))
         {
             anim.Play("mixamo_saltar");
+            GetComponentInParent<AudioSource>().PlayOneShot(jump);
+            GetComponentInParent<BoxCollider>().center = new Vector3(0, 2.5f, 0);
         }
         
         else if (Input.GetKeyDown(KeyCode.DownArrow) && anim.GetCurrentAnimatorStateInfo(0).IsName("mixamo_correr"))
--- 3DGame/Assets/Scripts/Collision.cs
+++ 3DGame/Assets/Scripts/Collision.cs
@@ -1,12 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Collision : MonoBehaviour
 {
+    public Animator anim;
+    public AudioClip coin;
+    private bool impact;
+    private bool dying;
+
+
+    private void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+        impact = false;
+        dying = false;
+    }
     private void OnTriggerEnter(Collider other)
     {
-        SceneManager.LoadScene("TempleRun");
+
+        if (other.gameObject.name.Equals("rock")) {
+            anim.Play("mixamo_muerte");
+            GetComponentInParent<Move>().speed = new Vector3(0, 0, 10);
+            impact = true;
+        }
+        else
+        {
+            GetComponent<AudioSource>().PlayOneShot(coin);
+        }
+        //SceneManager.LoadScene("TempleRun");
+    }
+
+    private void Update()
+    {
+        if (impact && anim.GetCurrentAnimatorStateInfo(0).IsName("mixamo_muerte"))
+        {
+            dying = true;
+        }
+        
+        else if (dying && !anim.GetCurrentAnimatorStateInfo(0).IsName("mixamo_muerte")) {
+            impact = false;
+            dying = false;
+            GetComponentInParent<Move>().speed = new Vector3(0, 0, 0);
+            //SceneManager.LoadScene("TempleRun");
+        }
+
+        if (dying)
+        {
+            GetComponentInParent<Move>().speed = GetComponentInParent<Move>().speed - new Vector3(0, 0, 0.02f);
+        }
     }
 }
